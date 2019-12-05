0f606241696cb5b8048c2db3490d1cc8f10c4d43
ShadowFoxNixill <github@nix.nixill.net>
2019-12-05 01:42:01 -0500

    Update DBTest to test Japanese instead of English

--- src/test/java/com/github/theoakemontrainers/oakedex/DBTest.java
+++ src/test/java/com/github/theoakemontrainers/oakedex/DBTest.java
@@ -17,15 +17,15 @@ public class DBTest {
       System.out.println(obj);
     }
     System.out.println();
-    System.out.print("Selecting language 9: ");
+    System.out.print("Selecting language 1: ");
     
     // test getResult();
     System.out.println(
-        conn.getResult("select identifier from languages where id = 9;"));
+        conn.getResult("select identifier from languages where id = 1;"));
     
     // test getRow()
     HashMap<String, Object> row = conn
-        .getRow("select * from languages where id = 9");
+        .getRow("select * from languages where id = 1");
     for (Entry<String, Object> ent : row.entrySet()) {
       System.out.println(ent.getKey() + ": " + ent.getValue().toString());
     }
@@ -34,7 +34,7 @@ public class DBTest {
     // test getMap()
     System.out.println("Names:");
     HashMap<Object, Object> map = conn.getMap(
-        "select local_language_id, name from language_names where language_id = 9");
+        "select local_language_id, name from language_names where language_id = 1");
     for (Entry<Object, Object> ent : map.entrySet()) {
       System.out.println(
           ent.getKey().toString() + ": " + ent.getValue().toString());
