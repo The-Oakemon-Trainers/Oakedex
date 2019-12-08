b7dddb41edfa672b9b1b7fbb3b35d7b828f0f5c1
ShadowFoxNixill <github@nix.nixill.net>
2019-12-08 14:59:51 -0500

    Start making search class

---
+++ src/main/java/com/github/theoakemontrainers/oakedex/dblookups/Search.java
@@ -0,0 +1,11 @@
+package com.github.theoakemontrainers.oakedex.dblookups;
+
+import net.nixill.databases.DBConnection;
+
+public class Search {
+  private DBConnection conn;
+  
+  public Search(DBConnection c) {
+    conn = c;
+  }
+}
