fa22f008580bb201560358ae559215b12b804d3b
ShadowFoxNixill <github@nix.nixill.net>
2019-12-08 18:56:59 -0500

    update Search for type filtering

--- src/main/java/com/github/theoakemontrainers/oakedex/dblookups/Search.java
+++ src/main/java/com/github/theoakemontrainers/oakedex/dblookups/Search.java
@@ -17,6 +17,18 @@ public class Search {
   private DBStatement searchAbilityHidden;
   private DBStatement searchAbilityFilter;
   private DBStatement searchAbilityEnd;
+  private DBStatement searchTypesStart;
+  private DBStatement searchTypesAtLeast;
+  private DBStatement searchTypesOneOf;
+  private DBStatement searchTypesOnly;
+  private DBStatement searchTypesOnlyOneOf;
+  private DBStatement searchTypesTwoOf;
+  private DBStatement searchTypesNot;
+  private DBStatement searchTypesNotJust;
+  private DBStatement searchTypesPrimarily;
+  private DBStatement searchTypesSecondarily;
+  private DBStatement searchTypesGetResults;
+  private DBStatement searchTypesEnd;
   private DBStatement results;
   
   public Search(DBConnection c) {
@@ -29,6 +41,19 @@ public class Search {
     searchAbilityHidden = c.prepare(Statements.SEARCH_ABILITIES_HIDDEN);
     searchAbilityFilter = c.prepare(Statements.SEARCH_ABILITIES_FILTER);
     searchAbilityEnd = c.prepare(Statements.SEARCH_ABILITIES_DROP);
+    searchTypesStart = c.prepare(Statements.SEARCH_TYPES_START);
+    searchTypesAtLeast = c.prepare(Statements.SEARCH_TYPES_AT_LEAST);
+    searchTypesOneOf = c.prepare(Statements.SEARCH_TYPES_ONE_OF);
+    searchTypesOnly = c.prepare(Statements.SEARCH_TYPES_ONLY);
+    searchTypesOnlyOneOf = c.prepare(Statements.SEARCH_TYPES_ONLY_ONE_OF);
+    searchTypesTwoOf = c.prepare(Statements.SEARCH_TYPES_TWO_OF);
+    searchTypesNot = c.prepare(Statements.SEARCH_TYPES_NOT);
+    searchTypesNotJust = c.prepare(Statements.SEARCH_TYPES_NOT_JUST);
+    searchTypesPrimarily = c.prepare(Statements.SEARCH_TYPES_PRIMARILY);
+    searchTypesSecondarily = c
+        .prepare(Statements.SEARCH_TYPES_SECONDARILY);
+    searchTypesGetResults = c.prepare(Statements.SEARCH_TYPES_GET_RESULTS);
+    searchTypesEnd = c.prepare(Statements.SEARCH_TYPES_END);
     results = c.prepare(Statements.SEARCH_EXECUTE);
   }
   
@@ -53,6 +78,75 @@ public class Search {
     
     conn.update(searchAbilityFilter);
     conn.update(searchAbilityEnd);
+    
+    return (int) conn.getResult(count);
+  }
+  
+  public int filterTypes(int mode, boolean normal, boolean fighting,
+      boolean flying, boolean poison, boolean ground, boolean rock,
+      boolean bug, boolean ghost, boolean steel, boolean fire,
+      boolean water, boolean grass, boolean electric, boolean psychic,
+      boolean ice, boolean dragon, boolean dark, boolean fairy) {
+    conn.update(searchTypesStart);
+    
+    String insertion = "";
+    if (normal) insertion += ", (1)";
+    if (fighting) insertion += ", (2)";
+    if (flying) insertion += ", (3)";
+    if (poison) insertion += ", (4)";
+    if (ground) insertion += ", (5)";
+    if (rock) insertion += ", (6)";
+    if (bug) insertion += ", (7)";
+    if (ghost) insertion += ", (8)";
+    if (steel) insertion += ", (9)";
+    if (fire) insertion += ", (10)";
+    if (water) insertion += ", (11)";
+    if (grass) insertion += ", (12)";
+    if (electric) insertion += ", (13)";
+    if (psychic) insertion += ", (14)";
+    if (ice) insertion += ", (15)";
+    if (dragon) insertion += ", (16)";
+    if (dark) insertion += ", (17)";
+    if (fairy) insertion += ", (18)";
+    
+    conn.update(
+        "INSERT INTO search_types VALUES " + insertion.substring(2) + ";");
+    
+    DBStatement typeStatement = null;
+    
+    switch (mode) {
+      case 1:
+        typeStatement = searchTypesAtLeast;
+        break;
+      case 2:
+        typeStatement = searchTypesOneOf;
+        break;
+      case 3:
+        typeStatement = searchTypesOnly;
+        break;
+      case 4:
+        typeStatement = searchTypesOnlyOneOf;
+        break;
+      case 5:
+        typeStatement = searchTypesTwoOf;
+        break;
+      case 6:
+        typeStatement = searchTypesNot;
+        break;
+      case 7:
+        typeStatement = searchTypesNotJust;
+        break;
+      case 8:
+        typeStatement = searchTypesPrimarily;
+        break;
+      case 9:
+        typeStatement = searchTypesSecondarily;
+        break;
+    }
+    
+    conn.update(typeStatement);
+    conn.update(searchTypesGetResults);
+    conn.update(searchTypesEnd);
     
     return (int) conn.getResult(count);
   }
